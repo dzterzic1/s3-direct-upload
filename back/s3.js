677600ec5a08501fa97eeabcada70f32d06a6293
dzenana <dterzic1@etf.unsa.ba>
2025-05-19 15:04:54 +0200

    azuriranje regije i naziva bucket-a

--- back/s3.js
+++ back/s3.js
@@ -1,20 +1,22 @@
 import dotenv from 'dotenv'
+dotenv.config()
 import aws from 'aws-sdk'
 import crypto from 'crypto'
 import { promisify } from "util"
 const randomBytes = promisify(crypto.randomBytes)
 
-dotenv.config()
 
-const region = "us-west-2"
-const bucketName = "direct-upload-s3-bucket-thing"
+const region = "us-east-1"
+const bucketName = "direct-photo-upload-bucket"
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
+const sessionToken = process.env.AWS_SESSION_TOKEN
 
 const s3 = new aws.S3({
   region,
   accessKeyId,
   secretAccessKey,
+  sessionToken,
   signatureVersion: 'v4'
 })
 
